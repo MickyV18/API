611fbf487dfcc362209d181d823058cd4961c0bc
MickyV18 <mickyvalentino18@gmail.com>
2024-11-12 05:34:47 +0700

    tst

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./test.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Ganti `SUPABASE_DATABASE_URL` dengan URL database dari Supabase Anda
+SUPABASE_DATABASE_URL = "postgresql://postgres.chjuivjgmjekpdgqlvji:@SupaMick18@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
+
+engine = create_engine(SUPABASE_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
